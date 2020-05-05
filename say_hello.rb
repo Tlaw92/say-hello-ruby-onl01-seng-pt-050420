313be7a483fc1230dd8b075b7da43074c186ffae
travis Lawson <travislawclimb@gmail.com>
2020-05-05 20:58:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
 # Build your say_hello method here
+
+#def say_hello (name)
+#  puts "Hello #{name}"
+#end
+
+#say_hello ("Kent Beck")  
+
+
+def greeting(name = "Kent Beck")
+  puts "Hello, #{name}"
+end
+
+greeting
